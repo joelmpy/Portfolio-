b758484607592edfda67db41863a60b110a918fe
Joel <joel_mpy@hotmail.com>
2022-04-23 12:27:35 +0200

    responsive

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-const txtAnime = document.querySelector('h2');
-console.log(txtAnime)
-new Typewriter (txtAnime, {
-    lopp : true,
-     deleteSpeed : 50,
-})
-.typeString("<span>Je suis un Développeur web junior</span>")
-.pauseFor(250)
-.deleteChars(11)
-.typeString("<span style = 'color:darkred'> FrondEnd</span>")
-.pauseFor(300)
-.deleteChars(8)
-.typeString("<span  style = 'color:darkred'> BackEnd</span>")
-.start()
+// const txtAnime = document.querySelector('h2');
+// console.log(txtAnime)
+// new Typewriter (txtAnime, {
+//     lopp : true,
+//      deleteSpeed : 50,
+// })
+// .typeString("<span>Je suis un Développeur web junior</span>")
+// .pauseFor(250)
+// .deleteChars(11)
+// .typeString("<span style = 'color:darkred'> FrondEnd</span>")
+// .pauseFor(300)
+// .deleteChars(8)
+// .typeString("<span  style = 'color:darkred'> BackEnd</span>")
+// .start()
